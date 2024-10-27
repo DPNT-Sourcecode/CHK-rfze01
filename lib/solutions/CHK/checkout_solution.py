38751521a74d7df68e37e923e5e7bfd892862379
sbx_user1051 <sbx_user1051@169.254.110.133>
2024-10-27 15:16:38 +0000

    Sun Oct 27 15:16:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,8 +6,16 @@ def checkout(skus):
     products = sorted([i.strip(" ") for i in skus])
     products = Counter(products)
     cost = 0
-    for sku, count in products.items:
-        if i not in ["A", "B", "C", "D"]:
+    for sku, count in products.items():
+        if sku not in ["A", "B", "C", "D"]:
             return -1
         else:
-            if
+            if sku == "A":
+                cost += (count // 3) * 130
+                cost += (count % 3) * 50
+            elif sku == "B":
+                cost += (count // 2) * 45
+                cost += (count % 2) * 30
+            elif sku == "C":
+                
+
