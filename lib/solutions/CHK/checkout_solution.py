c7411fc2939f2b7c922a783395267c2118f5fbf1
sbx_user1051 <sbx_user1051@169.254.110.133>
2024-10-27 15:13:38 +0000

    Sun Oct 27 15:13:38 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,13 @@
-
+from collections import Counter
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    products = sorted([i.strip(" ") for i in skus])
+    products = Counter(products)
+    cost = 0
+    for sku, count in products.items:
+        if i not in ["A", "B", "C", "D"]:
+            return -1
+        else:
+            if
