207c32bba46e69b5fb954177b399832dac4bef2a
sbx_user1051 <sbx_user1051@169.254.110.133>
2024-10-27 14:52:32 +0000

    Sun Oct 27 14:52:32 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if x.type()
-    if x > 100 or x < 0:
-        raise ValueError("numbers ")
+    if isinstance(x, int) and isinstance(y, int):
+        if x > 100 or x < 0:
+            raise ValueError("Input params between 0 and 100")
+        else:
+            return x + y
+    else:
+        raise ValueError("Input params are integers")
+
 
